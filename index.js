0ecca0939f86bd288cad9d8fc37a90a9872754d5
ALEXANDRE GABRIEL BONA <alexandre.bona3@alunos.sc.senac.br>
2025-05-14 21:10:21 -0300

    Adicionada funcao de adicionar usuario na tabela

--- index.js
+++ index.js
@@ -1,37 +1,59 @@
-let listaUsuarios = [];
+//Cria uma lista vazia para ser consultada pelo storage
+let listaDeUsuarios = [];
 
+//Funcao para adicionar os dados na tabela
 function adicionarUsuarioTabela(userNameValue, userEmailValue, userPasswordValue) {
+    //Cria uma nova linha na tabela
     const linhaNova = document.createElement('tr');
+    //Cria uma nova coluna (nome)
     const userTable = document.createElement('td');
     userTable.innerText = userNameValue;
+    //Cria uma nova coluna (email)
     const emailTable = document.createElement('td');
     emailTable.innerText = userEmailValue;
+    //Cria uma nova coluna (senha)
     const passwordTable = document.createElement('td');
     passwordTable.innerText = userPasswordValue;
-    const listaUsuarios = document.getElementById("table_form");
+    //Variável para adicionar os dados na tabela do html
+    const listaUsuarios = document.getElementById("tabela_usuarios");
+
+    //Utiliza a variável criada à cima para adicionar os dados às novas linhas da tabela
     listaUsuarios.appendChild(linhaNova);
     listaUsuarios.appendChild(userTable);
     listaUsuarios.appendChild(emailTable);
     listaUsuarios.appendChild(passwordTable);
-
-
 }
 
+//Funcao para obter os dados nos inputs
 function salvarUsuario() {
-    console.log("Botao funcionando")
+    //Obtém o nome do usuario informado no input
     const userName = document.getElementById("user_name");
     const userNameValue = userName.value;
+    //Obtém o email do usuario informado no input
     const userEmail = document.getElementById("user_email");
     const userEmailValue = userEmail.value;
+    //Obtém a senha do usuario informada no input
     const userPassword = document.getElementById("user_password");
     const userPasswordValue = userPassword.value;
-    console.log(userNameValue)
-    console.log(userEmailValue)
-    console.log(userPasswordValue)
+
+    //Chama a funcao para adicionar na tela
     adicionarUsuarioTabela(userNameValue, userEmailValue, userPasswordValue);
+
+    //Storage
+    listaDeUsuarios.push(userNameValue);
+    localStorage.setItem("tabela_usuarios", JSON.stringify(listaDeUsuarios));
+}
+
+function carregarUsuarios() {
+    const storage = JSON.parse(localStorage.getItem("tabela_usuarios"));
+    listaDeUsuarios = storage ? storage : [];
+    for (let usuarios of listaDeUsuarios) {
+        adicionarUsuarioTabela(usuarios);
+    }
 }
 
 function gerenciarEventos() {
+    carregarUsuarios();
     const botaoEnviar = document.getElementById("user_button");
     botaoEnviar.addEventListener("click", salvarUsuario);
 }
